8a3fc27c506190092658a2e7a2e8a861add76199
Savely <Savka9765@gmail.com>
2023-05-01 11:23:57 +0300

    HW3

---
+++ HW3/base/abstractClasses/ACheckQuantity.java
@@ -0,0 +1,6 @@
+package HW3.base.abstractClasses;
+
+import HW3.base.interfaces.ICheckQuantity;
+
+public abstract class ACheckQuantity implements ICheckQuantity {
+}
---
+++ HW3/base/abstractClasses/AFindTheSameFileName.java
@@ -0,0 +1,9 @@
+package HW3.base.abstractClasses;
+
+import HW3.base.interfaces.IFindTheSameFileName;
+
+import java.io.File;
+
+public abstract class AFindTheSameFileName implements IFindTheSameFileName {
+    protected File folder;
+}
